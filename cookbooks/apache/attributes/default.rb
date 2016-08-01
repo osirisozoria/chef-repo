872bad5c3fec84ee1554b3dfd24b76a5629f6784
root <root@wokstation.(none)>
2016-08-01 06:09:14 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,11 @@
+default["apache"]["sites"]["osirisozoria2"] = { "site_title" => "Osirisozoria2 website coming soon", "port" => 80, "domain" => "osirisozoria2.mylabserver.com" }
+default["apache"]["sites"]["osirisozoria2b"] = { "site_title" => "Osirisozoria2b website coming soon", "port" => 80, "domain" => "osirisozoria2b.mylabserver.com" }
+default["apache"]["sites"]["osirisozoria3"] = { "site_title" => "Osirisozoria3 website coming soon", "port" => 80, "domain" => "osirisozoria3.mylabserver.com" }
+default["apache"]["sites"]["osirisozoria3b"] = { "site_title" => "Osirisozoria3 website coming soon", "port" => 80, "domain" => "osirisozoria3b.mylabserver.com" }
+
+case node["platform"]
+	when "centos"
+	     	default["apache"]["package"] = "httpd"
+	when "ubuntu"
+		default["apache"]["package"] = "apache2"
+end
---
+++ cookbooks/security/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: security
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file "/etc/chef/validator.pem" do
+	action :delete
+end
+
